624c37adeb6a53e9ce44cb03bb2ce630f88e7b03
Ischadie-dobra <125760331+Ischadie-dobra@users.noreply.github.com>
2023-03-20 22:13:10 +0300

    Домашняя работа 7

---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+//Не понимаю, почему так много цифр после запятой выводит
+/*
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+
+
+double[,] FillMatrixWithRandom(int row, int column)
+{
+double[,] array = new double[row, column];
+Random rnd = new Random();
+for (int i = 0; i < row; i++)
+{
+for (int j = 0; j < column; j++)
+{
+array[i, j] = rnd.NextDouble();
+}
+}
+return array;
+}
+
+void PrintMatrix(double[,] array)
+{
+for (int i = 0; i < row; i++)
+{
+for (int j = 0; j < column; j++)
+{
+Console.Write($"{array[i,j]} \t");
+}
+Console.WriteLine();
+}
+}
+*/
+
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+/*
+int[,] FillMatrixWithRandom(int row, int column)
+{
+int[,] array = new int[row, column];
+Random rnd = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = rnd.Next(0, 10);
+}
+}
+return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i,j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+void FindNumber(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+{
+ for (int j = 0; j < array.GetLength(1); j++) 
+  {
+    if(row < 0 | row > array.GetLength(0) - 1 | column < 0 | column > array.GetLength(1) - 1)
+            {
+                Console.WriteLine("Элемент не существует");
+            }
+            else
+            {
+                Console.WriteLine($"{"Значение элемента массива =  "}");
+            }
+  }
+}
+Console.ReadLine();
+}
+*/
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+int[,] array = new int[row, column];
+Random rnd = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = rnd.Next(0, 10);
+}
+}
+return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i,j]} \t");
+}
+
+Console.WriteLine();
+}
+
+Console.WriteLine(array.GetLength(0));
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{ sum / array.GetLength(0)} ");
+}
+Console.ReadLine();
+}
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+
+
+
